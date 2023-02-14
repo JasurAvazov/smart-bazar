63beb899f68ab29a6832390ebb4f9c1f50172e85
JasurAvazov <jasuravazov4@gmail.com>
2023-02-14 21:01:52 +0500

    sarkor

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
 import * as functions from "./modules/functions.js";
+import * as wrapper from "./modules/wrapper.js";
+
 
 functions.isWebp();
+wrapper.init();
---
+++ src/js/modules/wrapper.js
@@ -0,0 +1,20 @@
+export function init() {
+    const openMain = document.querySelectorAll('.main-open')
+    openMain.forEach(el => {
+        el.addEventListener('click', () => {
+            
+        })
+    })
+
+
+    const foo = document.querySelectorAll('.foo')
+    foo.forEach(el => {
+        el.addEventListener('click', () => {
+            const sections = document.querySelectorAll('section')
+            sections.forEach(el => {
+                el.classList.remove('active')
+            })
+
+        })
+    })
+}
