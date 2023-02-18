fc730e59ccc396a19d486fd4ed820ab1cf9b7e10
JasurAvazov <jasuravazov4@gmail.com>
2023-02-18 16:32:44 +0500

    js

--- src/js/modules/wrapper.js
+++ src/js/modules/wrapper.js
@@ -1,34 +1,26 @@
 export function init() {
-    // const openMain = document.querySelectorAll('.main-open')
-    // openMain.forEach(el => {
-    //     el.addEventListener('click', () => {
+    const mainOpenBtns = document.querySelectorAll('.mainOpen')
+    const categoryOpenBtns = document.querySelectorAll('.categoryOpen')
 
-    //     })
-    // })
+    const wrappers = document.querySelectorAll('section')
+    const main = document.querySelector('.main')
+    const category = document.querySelector('.product')
 
-    const section = document.querySelectorAll('.wrapper>section')
-    const logo = document.querySelector('.header__logo')
-
-    const rout = document.querySelectorAll('.rout')
-    // rout.forEach(el => {
-    //     el.addEventListener('click', () => {
-    //         main.classList.remove('active')
-    //         category.classList.add('active')
-    //     })
-    // })
-    for (let i = 0; i < rout.length; i++) {
-        rout[i].addEventListener('click', function () {
-            for (let j = 0; j < section.length; j++) {
-                section[0].classList.remove('active')
-                section[1].classList.add('active')
-            }
+    mainOpenBtns.forEach(el => {
+        el.addEventListener('click', () => {
+            wrappers.forEach(sec => {
+                sec.classList.remove('active')
+            })
+            main.classList.add('active')
         })
-    }
+    })
 
-    logo.addEventListener('click', function () {
-        for (let j = 0; j < section.length; j++) {
-            section[0].classList.add('active')
-            section[1].classList.remove('active')
-        }
+    categoryOpenBtns.forEach(el => {
+        el.addEventListener('click', () => {
+            wrappers.forEach(sec => {
+                sec.classList.remove('active')
+            })
+            category.classList.add('active')
+        })
     })
 }
