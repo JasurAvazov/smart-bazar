d317025267d50583439b1ec616febf508175111d
Sarkor2007 <abilkosimovsarkor@gmail.com>
2023-02-18 22:11:36 +0500

    add news section

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,5 +1,34 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
 
 export function init() {
-    
+    var swiper = new Swiper(".mySwiper", {
+        modules: [Pagination, Navigation, Autoplay],
+        slidesPerView: 1,
+        spaceBetween: 16,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            576: {
+                slidesPerView: 2,
+                spaceBetween: 16,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 16,
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 16,
+            },
+            1200: {
+                slidesPerView: 5,
+                spaceBetween: 16,
+            }
+        },
+        autoplay: {
+            delay: 2000,
+        },
+    });
 }
